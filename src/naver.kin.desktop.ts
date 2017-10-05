2bf042032857d1e03dac5db11369748e583919c6
JaeHo Song <thruthesky@gmail.com>
2017-10-06 03:09:29 +0800

    update

---
+++ src/naver.kin.desktop.ts
@@ -0,0 +1,31 @@
+const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
+const argv = require('yargs');
+
+import { PuppeteerExtension } from './puppeteer-extension/peppeteer-extension';
+
+class NaverKinDeskstop extends PuppeteerExtension {
+    constructor() {
+        super();
+    }
+    async main() {
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+        this.set( browser, page );
+
+        await this.page.goto('https://www.philgo.com');
+        await this.getTitle();
+        await this.getHtmlTitle();
+
+        await this.browser.close();
+    }
+    async getTitle() {
+        console.log('title: ', await this.page.title() );
+    }
+    async getHtmlTitle() {
+        const $html = await this.html();
+        console.log('html title: ', $html.find('title').text())
+    }
+}
+
+(new NaverKinDeskstop()).main();
